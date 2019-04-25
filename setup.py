43614118c900a9a2ec148f783679145d87f6a84f
Celine Wei <c.wei@surgicalsafety.com>
2019-04-25 15:37:34 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='IOHandler',
+      version='1.0',
+      description='To handle IO related enqueing, dequing',
+      url='',
+      author='Celine Wei',
+      author_email='cw.git.general@gmail.com',
+      license='MIT',
+      packages=['IOHandler'],
+      install_requires=[],
+      dependency_links=[],
+      zip_safe=False,)
